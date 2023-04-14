74086ced94e52c2d771b2fe0afef344bc17e9ffe
rabbitfighter81 <rabbitfighter81@gmail.com>
2023-04-13 22:57:18 -0700

    Added more validation in sign up

--- lib/auth/sign_up.dart
+++ lib/auth/sign_up.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:js_util';
+
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:wordsleuth/auth/confirm_account.dart';
 import 'package:wordsleuth/auth/sign_in.dart';
+import 'package:email_validator/email_validator.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key, required this.title});
@@ -71,154 +74,168 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-              backgroundColor: Colors.deepPurple,
-              foregroundColor: Colors.white,
-            ),
-            body: ListView(
-              shrinkWrap: false,
-              padding: const EdgeInsets.all(15.0),
-              children: <Widget>[
-                const SizedBox(
-                  height: 25.0,
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Column(
-                    children: <Widget>[
-                      const Text('Sign Up',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 20)),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                      TextFormField(
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter a username';
-                          } else {
-                            return null;
-                          }
-                        },
-                        controller: usernameController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.person),
-                          labelText: "Username",
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                      TextFormField(
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your email';
-                          } else {
-                            return null;
-                          }
-                        },
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.email),
-                          labelText: "Email",
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                      TextFormField(
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter a password';
-                          } else {
-                            return null;
-                          }
-                        },
-                        controller: passwordController,
-                        obscureText: !passwordVisible,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            prefixIcon: const Icon(Icons.lock),
-                            labelText: "New Password",
-                            suffixIcon: IconButton(
-                                onPressed: _togglePasswordVisible,
-                                icon: Icon(!passwordVisible
-                                    ? Icons.visibility_off
-                                    : Icons.visibility))),
-                      ),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                      TextFormField(
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please verify your password';
-                          } else {
-                            return null;
-                          }
-                        },
-                        controller: passwordConfirmController,
-                        obscureText: !passwordVisible,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.lock),
-                          labelText: "Verify Password",
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                      MaterialButton(
-                        onPressed: () {
-                          // Validate returns true if the form is valid, or false otherwise.
-                          if (_formKey.currentState!.validate()) {
-                            if (usernameController.text.isNotEmpty &&
-                                emailController.text.isNotEmpty &&
-                                passwordController.text.isNotEmpty &&
-                                passwordConfirmController.text.isNotEmpty &&
-                                (passwordController.text ==
-                                    passwordConfirmController.text)) {
-                              signUpUser(context);
-                            }
-                          }
-                        },
-                        minWidth: 350.0,
-                        height: 50.0,
-                        color: Colors.deepPurple,
-                        child: const Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25.0,
-                      ),
-                    ],
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+        body: Form(
+            key: _formKey,
+            child: ListView(
+                shrinkWrap: false,
+                padding: const EdgeInsets.all(15.0),
+                children: <Widget>[
+                  const SizedBox(
+                    height: 25.0,
                   ),
-                )
-              ],
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                // Validate returns true if the form is valid, or false otherwise.
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SignInPage(title: widget.title)));
-              },
-              tooltip: 'Go Home',
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.deepPurple,
-              child: const Icon(Icons.home),
-            )));
+                  const Text('Sign Up',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a username';
+                      } else {
+                        if (value.length < 8) {
+                          return 'Must be 8 or more characters';
+                        }
+                        return null;
+                      }
+                    },
+                    autovalidateMode: AutovalidateMode.always,
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                      labelText: "Username",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      } else {
+                        if (!EmailValidator.validate(value)) {
+                          return 'Must be a valid email address';
+                        }
+
+                        return null;
+                      }
+                    },
+                    autovalidateMode: AutovalidateMode.always,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                      labelText: "Email",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      } else {
+                        if (value.length < 6) {
+                          return 'Must be 6 or more characters';
+                        }
+                        return null;
+                      }
+                    },
+                    autovalidateMode: AutovalidateMode.always,
+                    controller: passwordController,
+                    obscureText: !passwordVisible,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        prefixIcon: const Icon(Icons.lock),
+                        labelText: "New Password",
+                        suffixIcon: IconButton(
+                            onPressed: _togglePasswordVisible,
+                            icon: Icon(!passwordVisible
+                                ? Icons.visibility_off
+                                : Icons.visibility))),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please verify your password';
+                      } else {
+                        if (value.length < 6) {
+                          return 'Must be 6 or more characters';
+                        }
+                        if (value.length >= 6 &&
+                            equal(passwordController.text,
+                                passwordConfirmController.text)) {
+                          return 'Passwords must match';
+                        }
+                        return null;
+                      }
+                    },
+                    autovalidateMode: AutovalidateMode.always,
+                    controller: passwordConfirmController,
+                    obscureText: !passwordVisible,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: "Verify Password",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        if (usernameController.text.isNotEmpty &&
+                            emailController.text.isNotEmpty &&
+                            passwordController.text.isNotEmpty &&
+                            passwordConfirmController.text.isNotEmpty &&
+                            (passwordController.text ==
+                                passwordConfirmController.text)) {
+                          signUpUser(context);
+                        }
+                      }
+                    },
+                    minWidth: 350.0,
+                    height: 50.0,
+                    color: Colors.deepPurple,
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  )
+                ])),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            // Validate returns true if the form is valid, or false otherwise.
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SignInPage(title: widget.title)));
+          },
+          tooltip: 'Go Home',
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.deepPurple,
+          child: const Icon(Icons.home),
+        ));
   }
 }
